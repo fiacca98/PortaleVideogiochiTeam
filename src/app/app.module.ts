e926dd9378c1b2aa7c6308ecaf4e58b741c3c92e
Gabriele Moia <gabriele.moia@icloud.com>
2018-03-21 10:31:00 +0100

    Creazione GameItem Class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
-import { AppComponent } from './app.component';
+import { AppComponent } from './Components/app.component';
+import { ListComponent } from './Components/list/list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule
