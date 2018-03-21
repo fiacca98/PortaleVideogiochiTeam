f40e09d20a790efe4bf07606328261cafbd32a55
Gabriele Moia <gabriele.moia@icloud.com>
2018-03-21 11:03:29 +0100

    Creazione DetailComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './Components/app.component';
 import { ListComponent } from './Components/list/list.component';
-import {GameListService} from './Services/game-list.service'
+import {GameListService} from './Services/game-list.service';
+import { DetailComponent } from './Components/detail/detail.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListComponent
+    ListComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule
