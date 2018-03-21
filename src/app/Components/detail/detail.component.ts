cecd88c68f391ca7c97a04f328e0c1a43435ab9d
Gabriele Moia <gabriele.moia@icloud.com>
2018-03-21 11:13:07 +0100

    Sviluppo Detail Component Completo

--- src/app/Components/detail/detail.component.ts
+++ src/app/Components/detail/detail.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router, ActivatedRoute } from '@angular/router';
+import { GameListService } from '../../Services/game-list.service';
+import { GameItem } from '../../Beans/game-item'
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
   styleUrls: ['./detail.component.css']
 })
 export class DetailComponent implements OnInit {
+  item: GameItem;
+  id: String;
 
-  constructor() { }
+  constructor(private route: Router, private gameListService: GameListService, private router: ActivatedRoute) {
+    this.router.params.subscribe(params => {//
+      if (params['id'] != '' && params['id'] != null) {
+        this.item = this.gameListService.getElementById(params['id']);
+      }
+    });
+  }
 
   ngOnInit() {
+  }
+
+  goToEdit() {
+    this.route.navigate(['/edit/']);
+  }
+
+  goToList() {
+    this.route.navigate(['/list/']);
   }
 
 }
