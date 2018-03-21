dff223cef9d29554a85751be50cdf5540716501a
vanessalandauro <landauro.kelly@gmail.com>
2018-03-21 11:59:42 +0100

    modifica logica login

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,44 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
-import { LoginService } from '../../Services/login.service';
+import { Component, OnInit } from "@angular/core";
+import { Router, ActivatedRoute } from "@angular/router";
+import { LoginService } from "../../Services/login.service";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"]
 })
 export class LoginComponent implements OnInit {
   username: string;
   password: string;
-  admin: boolean;
-  constructor(private loginService: LoginService, private router: Router) {
-  }
+  constructor(private loginService: LoginService, private router: Router) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   login() {
-
-    switch(this.loginService.checkData(this.username, this.password)){
-
+    let isAdmin: boolean = false;
+    switch (this.loginService.checkData(this.username, this.password)) {
       case 0:
-      sessionStorage.setItem("username", this.username);
-      sessionStorage.setItem("password", this.password);
-      sessionStorage.setItem("admin", "yes");
-      this.router.navigate(["/home/"]);
-      break;
-
+        isAdmin = true;
+        break;
       case 1:
-      sessionStorage.setItem("username", this.username);
-      sessionStorage.setItem("password", this.password);
-      this.router.navigate(["/home/"]);
-      break;
-
+        break;
       case 2:
-      alert("utente non registrato");
-      break;
-
+        alert("utente non registrato");
+        return;
+      default:
+        console.log("valore checkData non corrisposto ");
+        return;
     }
+    sessionStorage.setItem("username", this.username);
+    sessionStorage.setItem("password", this.password);
+    if (isAdmin) {
+      sessionStorage.setItem("admin", "yes");
+    }
+    this.router.navigate(["/home/"]);
   }
-
 }
